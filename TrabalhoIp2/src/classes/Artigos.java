a935d204a398ff9ee78d406bebae3c2e8989f79f
JM18JM <155195351+JM18JM@users.noreply.github.com>
2023-12-29 22:46:37 +0000

    Update Artigos.java
    a

--- TrabalhoIp2/src/classes/Artigos.java
+++ TrabalhoIp2/src/classes/Artigos.java
@@ -12,6 +12,15 @@ public class Artigos {
 
         
     }
-    
+    public void listarInformacaoArtigo (){
+     System.out.println("quantidade"+quantidade);
+System.out.println("nome"+nome);
+        System.out.println("id"+id);
+System.out.println("precovenda"+precovenda);
+        System.out.println("precocompra"+precocompra);
+System.out.println("marca"+marca);
+System.out.println("tipo"+tipo);
+        System.out.println("artigos"+artigo);
+    }
 }
 
